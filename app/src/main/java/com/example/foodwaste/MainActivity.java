044b2294c8898ceb0f4b4c097e8e3d4dafa49838
Shahad-123 <sh.a.alyafai!@gmail.com>
2023-06-02 16:26:45 +0400

    finished project

---
+++ app/src/main/java/com/example/foodwaste/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.foodwaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout searchfood,donatefood,updatefood,mycart,myorder,createcity,createplace,myprofile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchfood=findViewById(R.id.searchfood);
+        donatefood=findViewById(R.id.donatefood);
+        updatefood=findViewById(R.id.updatefood);
+        mycart=findViewById(R.id.mycats);
+        myorder=findViewById(R.id.myorder);
+        createcity=findViewById(R.id.createcity);
+        createplace=findViewById(R.id.createplace);
+        myprofile=findViewById(R.id.myprofile);
+       searchfood.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this,searchfood.class);
+               startActivity(intent);
+           }
+       });
+        donatefood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,donatefood.class);
+                startActivity(intent);
+            }
+        });
+        updatefood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,updatefood.class);
+                startActivity(intent);
+            }
+        });
+        mycart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,mycart.class);
+                startActivity(intent);
+            }
+        });
+        myorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,myorder.class);
+                startActivity(intent);
+            }
+        });
+        createcity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,createcity.class);
+                startActivity(intent);
+            }
+        });
+        createplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,createplace.class);
+                startActivity(intent);
+            }
+        });
+        myprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,myprofile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/foodwaste/Register.java
@@ -0,0 +1,26 @@
+package com.example.foodwaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+    Button Registerbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Registerbtn=findViewById(R.id.Register);
+        Registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,Long.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
